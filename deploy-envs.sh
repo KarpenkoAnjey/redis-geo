746be2901477f4628d55c6871eb82ddcaf316fac
KarpenkoAnjey <karpenko.anjey@gmail.com>
2017-07-18 09:05:38 +0300

    fix

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-2b
+export AWS_DEFAULT_REGION=us-east-2a
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=redisgeo4536453.netcore.io
 
