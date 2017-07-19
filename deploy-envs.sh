e9bc4c2c395da4817fd9df321ed387eda97e2aef
KarpenkoAnjey <karpenko.anjey@gmail.com>
2017-07-19 12:16:35 +0300

    ss

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,6 +4,7 @@
 export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
+
 export AWS_DEFAULT_REGION=us-east-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST = ec2-13-58-60-220.us-east-2.compute.amazonaws.com
